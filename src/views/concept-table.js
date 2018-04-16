870326add8f258096c931d1f6a6e5796a760c7fa
Sam Fentress <sfentress@concord.org>
2018-04-16 14:19:54 -0400

    Add concept popups to column headers

--- src/views/concept-table.js
+++ src/views/concept-table.js
@@ -1,12 +1,15 @@
 import { Table, Column, ColumnGroup, Cell } from 'fixed-data-table-2';
+import { PortalWithState } from 'react-portal';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Dimensions from 'react-dimensions';
+import {StyleSheet, css} from 'aphrodite';
 import { StudentNameCell, ConceptCell } from './cells';
 import '../css/fixed-data-table.css';
 import '../css/gem-table.css';
 
 const headingHeight = 19;
+const popupWidth = 350;
 
 class ConceptTable extends Component {
   constructor(props) {
@@ -14,10 +17,12 @@ class ConceptTable extends Component {
 
     this.subRowHeightGetter = this.subRowHeightGetter.bind(this);
     this.rowExpandedGetter = this.rowExpandedGetter.bind(this);
+    this.hideConceptPopup = this.hideConceptPopup.bind(this);
 
     this.state = {
       stackGems: true,
-      showChart: false
+      showChart: false,
+      conceptPopup: null
     };
   }
 
@@ -40,8 +45,22 @@ class ConceptTable extends Component {
     return null;
   }
 
+  showConceptPopup(concept) {
+    this.setState(previousState => ({
+      conceptPopup: concept,
+      newConceptPopup: (previousState.conceptPopup && previousState.conceptPopup !== concept)
+    }));
+  }
+
+  hideConceptPopup() {
+    if (this.state.newConceptPopup) {
+      this.setState({newConceptPopup: null});
+    } else {
+      this.setState({conceptPopup: null});
+    }
+  }
+
   createColumns() {
-    console.log("create columns!")
     const {
       dataStore
     } = this.props;
@@ -74,7 +93,11 @@ class ConceptTable extends Component {
       <Column
         columnKey={k}
         header={
-          <Cell>
+          <Cell
+            id={`header-${concept.label.short}`}
+            className={css(styles.clickable)}
+            onClick={() => this.showConceptPopup(concept)}
+            >
             {concept.label.short}
           </Cell>
         }
@@ -104,6 +127,41 @@ class ConceptTable extends Component {
     const width = Math.max(containerWidth, 430);
     const height = containerHeight;
 
+    let popup;
+    if (this.state.conceptPopup && this.state.conceptPopup.label.description) {
+      const concept = this.state.conceptPopup;
+      popup = (
+        <PortalWithState closeOnOutsideClick closeOnEsc defaultOpen onClose={this.hideConceptPopup}>
+          {({ portal, isOpen, openPortal }) => {
+            if (!isOpen) openPortal();
+            const cell = document.getElementById(`header-${concept.label.short}`);
+            const box = cell.getBoundingClientRect();
+            const style = {
+              position: 'absolute',
+              top: box.y + box.height + 5,
+              left: (box.x + (box.width / 2)) - (popupWidth / 2)
+            };
+            let locationStyle = "";
+            if (style.left < 50) {
+              style.left += 50;
+              locationStyle = "concept-popup-left";
+            } else if (style.left > 600) {
+              style.left -= 50;
+              locationStyle = "concept-popup-right";
+            }
+            const title = concept.label.longer;
+            const text = concept.label.description;
+            return portal(
+              <div style={style} className={`${css(styles.popup)} concept-popup ${locationStyle}`}>
+                <h1>{title}</h1>
+                {text}
+              </div>
+            );
+          }}
+        </PortalWithState>
+      );
+    }
+
     return (
       <div>
         <Table
@@ -120,10 +178,20 @@ class ConceptTable extends Component {
         >
           {columns}
         </Table>
+        {popup}
       </div>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  clickable: {
+    cursor: 'pointer'
+  },
+  popup: {
+    width: popupWidth
+  }
+});
 
 ConceptTable.propTypes = {
   dataStore: PropTypes.object,
